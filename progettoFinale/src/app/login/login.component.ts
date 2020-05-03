30edbea54f8f49629ce9ff41a2d8eb2ee64236c0
corradomarco <corrado.marco@iisgalvanimi.edu.it>
2020-05-03 15:25:59 +0000

    c

--- progettoFinale/src/app/login/login.component.ts
+++ progettoFinale/src/app/login/login.component.ts
@@ -1,41 +1,41 @@
+
+
+
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Utente } from '../user';
-
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  @Input() loggedUser : Utente;
-  @Input() utenti : Utente[];
-  @Output() userLogin = new EventEmitter<Utente>();
- formLog : FormGroup;
-  constructor(fb : FormBuilder) {
-    this.formLog = fb.group(
-      {
-        username : ['', Validators.required],
-        password : ['', Validators.required]
-      }
-    )
+export class LoginComponent {
+  title = 'progetto';
+  obsUnit: Observable<Utente[]>; //L’observable che sta in attesa dei dati
+  data: Utente[];
+  constructor(private http: HttpClient) { } //Dependency injection
+  getUnitList(): void {
+    //Qui va sostituito l’url con quello delle vostre api
+    this.obsUnit = this.http.get<Utente[]>('https://3000-db1efc4d-8341-4f10-97b1-772b75753341.ws-eu01.gitpod.io/users');
+    //Mi sottoscrivo all’observable e scrivo la arrow function che riceve i dati
+    this.obsUnit.subscribe((data: Utente[]) => {this.data = data;});
   }
-  login()
-  {
-    for(let i = 0; i < this.utenti.length; i++)
-    {
-      if(this.formLog.controls['username'].value == this.utenti[i].username &&
-        this.formLog.controls['password'].value == this.utenti[i].password)
-      {
-        this.loggedUser = this.utenti[i];
-        this.userLogin.emit(this.loggedUser);
-        console.log(this.loggedUser);
-        break;
-      }
+  postObserver : Observable<Object>;
+postData : Object;
+addUnit(Username: HTMLInputElement,Password:HTMLInputElement): boolean {
+    this.postData={
+      Username:Username.value,
+      Password:Password.value
     }
+
+
+    let headers =  {headers: new HttpHeaders().set('Content-Type', 'application/json')};
+    this.postObserver = this.http.post('https://3000-db1efc4d-8341-4f10-97b1-772b75753341.ws-eu01.gitpod.io/users', JSON.stringify(this.postData),headers)
+    this.postObserver.subscribe(data => this.postData = data);
+    return false;
   }
 
-  ngOnInit() {
-  }
 
 }
